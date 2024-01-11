3a31f338abd26f7b481f7c28b188eb413c09c7a3
Tatiana Barbosa <tstianabarbosa@gmail.com>
2024-01-10 21:03:20 -0500

    restaurado

---
+++ variable.py
@@ -0,0 +1,17 @@
+cadena = "Hola, "
+entero = 10
+flotante = 20.0
+booleano = True
+
+# Concatenar las cadenas 
+resultado = cadena + str(entero) + " " + str(flotante) + " " + str(booleano)
+
+# Los flotantes en Python son representados internamente como números de punto flotante de doble precisión (64 bits) según el estándar IEEE 754. 
+# El rango máximo es aproximadamente 1.8 x 10^308 y el mínimo es 2.2 x 10^-308 para números positivos.
+
+#formula de los  primeros n números pares
+suma_pares = entero * (entero + 1)
+
+print(resultado)
+print("Suma de los primeros", entero, "números pares:", suma_pares)
+
